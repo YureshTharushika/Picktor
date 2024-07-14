1fd1a366a3d4b05b1a721ba2b7c81bc3732c5d53
Yuresh Tharushika <yuresh.official@gmail.com>
2024-07-15 00:39:19 +0530

    added rotate funtionality

--- Program.cs
+++ Program.cs
@@ -3,19 +3,33 @@ using Discord;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Processing;
 using Image = SixLabors.ImageSharp.Image;
+using Microsoft.Extensions.Configuration;
 
 namespace Picktor
 {
     internal class Program
     {
         private readonly DiscordSocketClient _client;
-        private readonly string _token = "";
+        private readonly string _token;
 
         public Program()
         {
             _client = new DiscordSocketClient();
             _client.Log += LogAsync;
             _client.MessageReceived += MessageReceivedAsync;
+
+            // Build configuration
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            _token = configuration["Discord:Token"];
+
+            if (string.IsNullOrEmpty(_token))
+            {
+                throw new Exception("Bot token is not set in the configuration file.");
+            }
         }
 
         public async Task MainAsync()
@@ -89,6 +103,40 @@ namespace Picktor
                         outputStream.Seek(0, SeekOrigin.Begin);
 
                         await message.Channel.SendFileAsync(outputStream, "grayscale.png");
+                    }
+                }
+                else
+                {
+                    await message.Channel.SendMessageAsync("Please attach an image.");
+                }
+            }
+            else if (message.Content.StartsWith("!rotate"))
+            {
+                if (message.Attachments.Count > 0)
+                {
+                    var parts = message.Content.Split(' ');
+                    if (parts.Length == 2 && float.TryParse(parts[1], out float angle))
+                    {
+                        var attachment = message.Attachments.First();
+                        var imageUrl = attachment.Url;
+
+                        using (var client = new HttpClient())
+                        {
+                            var response = await client.GetAsync(imageUrl);
+                            var imageStream = await response.Content.ReadAsStreamAsync();
+                            var image = Image.Load(imageStream);
+                            image.Mutate(x => x.Rotate(angle));
+
+                            var outputStream = new MemoryStream();
+                            image.SaveAsPng(outputStream);
+                            outputStream.Seek(0, SeekOrigin.Begin);
+
+                            await message.Channel.SendFileAsync(outputStream, "rotated.png");
+                        }
+                    }
+                    else
+                    {
+                        await message.Channel.SendMessageAsync("Usage: !rotate <angle> and attach an image.");
                     }
                 }
                 else
